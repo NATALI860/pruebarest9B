2e3d48f2d6e000fa96bf235971429bad4ab86ff4
NATALI860 <2236000465@utna.edu.mx>
2025-06-18 16:51:45 -0600

    Este es mi primer commit de mi apirest 9B

---
+++ server.js
@@ -0,0 +1,9 @@
+const app = require('./app/app')
+const config = require('./app/config/configuracion')
+
+
+app.listen (config.PORT,()=>{
+    console.log("Aplicacion corriendo en puerto ",
+    config.PORT);
+    
+})
